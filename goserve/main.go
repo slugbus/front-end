c70ecffdd701f111a16e8beba75129ca8cdf05c6
Jorge Henriquez <joahenri@ucsc.edu>
2019-01-19 11:02:34 -0800

    write simple giveData api route

--- goserve/main.go
+++ goserve/main.go
@@ -48,6 +48,16 @@ func testGet(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func giveData(w http.ResponseWriter, r *http.Request) {
+	data, err := json.Marshal(bus.CurrentBusState)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	w.Write(data)
+}
+
 func main() {
 	var wait time.Duration
 	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
@@ -55,6 +65,7 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/api/test_get", testGet).Methods("GET")
+	r.HandleFunc("/api/get_buses", giveData).Methods("GET")
 
 	srv := &http.Server{
 		Addr: ":8080",

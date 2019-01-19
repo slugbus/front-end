c10a4a70823f07541e462734c918938154ce9177
Justyn <justynduthler@gmail.com>
2019-01-18 23:48:00 -0800

    Added distance function

--- goserve/internal/util/util.go
+++ goserve/internal/util/util.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"net/http"
 )
 
@@ -32,10 +33,41 @@ type BusData struct {
 
 type BusDataPlusPlus struct {
 	BusData
-	Speed       float64 `json:"speed"`
-	ISClockwise bool    `json:"isclockwise"`
+	Speed float64 `json:"speed"`
+	Angle float64 `json:"angle"`
 }
 
+func getDir(lat1, long1, lat2, long2 float64) float64 {
+	top := math.Log(math.Tan((lat2 / 2) + (math.Pi / 4)))
+	bottom := math.Log(math.Tan((lat1 / 2) + (math.Pi / 4)))
+	dTeta := top - bottom
+
+	dLong := math.Abs(long1 - long2)
+	teta := math.Atan2(dLong, dTeta)
+
+	return math.Round((180 / math.Pi) * teta)
+}
+
+func getDistlat1, long1, lat2, long2 float64) float64 {
+	lat1 = lat1 * math.Pi / 180
+	long1 = long1 * math.Pi / 180
+
+	lat2 = lat2 * math.Pi / 180
+	long2 = long2 * math.Pi / 180
+
+	dlon := long2 - long1
+	dlat := lat2 - lat1
+
+	a := math.Pow(math.Sin(dlat/2), 2) + math.Cos(lat1)*math.Cos(lat2)*math.Pow(math.Sin(dlon/2), 2)
+	c := 2 * math.Asin(math.Sqrt(a))
+
+	r := float64(6371)
+
+	kilo := c * r
+	miles := kilo * 0.621371
+
+	return miles
+}
 // SlugResponse is a collection
 // of BusData
 type SlugResponse []BusData

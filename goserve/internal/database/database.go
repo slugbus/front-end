26c7b2a480bf612fc1f72f009bb9a18f34dd482b
Jorge Henriquez <joahenri@ucsc.edu>
2019-01-20 04:09:44 -0800

    clairfy db

--- goserve/internal/database/database.go
+++ goserve/internal/database/database.go
@@ -11,18 +11,20 @@ import (
 	"google.golang.org/api/option"
 )
 
+// Client is a an authenticated reference to our fire
+// store db
 var Client *firestore.Client
 
+// This init function logs in to firebase
+// and sets Client
 func init() {
 	opt := option.WithCredentialsFile(".keys/firebase.json")
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		log.Printf("error initializing app: %v", err)
 	}
-
 	Client, err = app.Firestore(context.Background())
 	if err != nil {
 		log.Printf("error initializing app: %v", err)
 	}
-
 }

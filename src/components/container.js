761c353618b50f54053769e22315de9d7613b026
JohnnyHanly <johnnymhanly@gmail.com>
2019-01-20 07:18:50 -0800

    adding bus modal

--- src/components/container.js
+++ src/components/container.js
@@ -1,36 +1,13 @@
 import React from 'react';
 import MapView from './mapView';
 import axios from 'axios';
+import '../index.css'
 
 class Container extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            busArray: [
-                {
-                    angle: 0,
-                    id: "84",
-                    lat: 36.98868,
-                    lgn: -122.05421,
-                    speed: 0,
-                    type: "LOOP"
-                },
-                {
-                    angle: 0,
-                    id: "85",
-                    lat: 36.977905,
-                    lgn: -122.05425,
-                    speed: 0,
-                    type: "OUT OF SERVICE"
-                }, {
-                    angle: 109,
-                    id: "97",
-                    lat: 36.996716,
-                    lgn: -122.055405,
-                    speed: 1.047,
-                    type: "LOOP"
-                }
-            ],
+            busArray: [],
             lat: 0,
             lng: 0
         }
@@ -38,21 +15,36 @@ class Container extends React.Component {
 
     componentDidMount() {
 
-        axios.get("http://f52fa470.ngrok.io/api/get_buses")
+
+        this.intervalId = setInterval(() => this.updateBusMarkers(), 3000);
+        this.updateBusMarkers()
+
+    }
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+    }
+
+
+
+    updateBusMarkers() {
+        axios.get("http://35.233.194.110:8080/api/get_buses")
             .then(
                 (response) => {
-
+                    console.log("RESPONSE", response.data)
                     this.setState({
+                         busArray: [...response.data]
+
                     })
                 }
             )
-
     }
 
     render() {
         return (
-    
+           
                 <MapView busArray={this.state.busArray}></MapView>
+
+           
 
         )
     }

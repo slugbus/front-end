41b5a9f12b74250c5ee7835c0459a905e951b94d
Jorge Henriquez <joahenri@ucsc.edu>
2019-01-20 03:34:38 -0800

    Move defintions of bus into seperate file

---
+++ goserve/internal/bus/define.go
@@ -0,0 +1,32 @@
+package bus
+
+// CurrentBusState is the current
+// the state of the buses.
+var CurrentBusState SlugResponsePlusPlus
+
+// Data is a structure that
+// contains the json response
+// from the ucsc taps server.
+type Data struct {
+	ID   string  `json:"id"`
+	Lon  float64 `json:"lng"`
+	Lat  float64 `json:"lat"`
+	Type string  `json:"type"`
+}
+
+// SlugResponse is a collection
+// of BusData
+type SlugResponse []Data
+
+// DataPlusPlus is a structure that
+// contains data from Bus but with more
+// info
+type DataPlusPlus struct {
+	Data
+	Speed float64 `json:"speed"`
+	Angle float64 `json:"angle"`
+}
+
+// SlugResponsePlusPlus is a collection of
+// of BusDataPlusPlus
+type SlugResponsePlusPlus []DataPlusPlus
